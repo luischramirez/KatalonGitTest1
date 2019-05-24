19062a03ce5c6e381ca7b0964fedfbaae3adb210
Equipo 9 <Equipo 9@DESKTOP-24D2EAK>
2019-05-24 15:48:13 -0500

    Agregando modificación de pruebas

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>aed92b7c-519d-45da-adf4-30e947018b69</testSuiteGuid>
+   <testCaseLink>
+      <guid>5ee180ff-a92a-496a-b9e2-e97058f9901a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by David</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9412e438-e8a6-468c-8de6-2b5085f89da4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Luis</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
